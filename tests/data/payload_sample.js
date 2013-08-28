131c25b7c1ae3eaa082579ccdffae8eea15b3cd6
Olivier Yiptong <olivier@olivieryiptong.com>
2013-08-28 15:15:48 -0400

    adding payload test data

---
+++ tests/data/payload_sample.js
@@ -0,0 +1,380 @@
+var UPTestData = UPTestData || {};
+
+UPTestData.payloadSample = {
+  "content": {
+    "requestingSites": [
+      {
+        "interests": [
+          "Technology", 
+          "Android", 
+          "Apple", 
+          "Arts", 
+          "Autos"
+        ], 
+        "isBlocked": false, 
+        "isPrivileged": false, 
+        "name": "slashdot.org"
+      }
+    ], 
+    "interestsHosts": {
+      "Autos": [
+        {
+          "host": "caranddriver.com", 
+          "days": 2, 
+          "visits": 16
+        }
+      ], 
+      "Politics": [
+        {
+          "host": "salon.com", 
+          "days": 3, 
+          "visits": 22
+        }
+      ], 
+      "Sports": [
+        {
+          "host": "cbssports.com", 
+          "days": 1, 
+          "visits": 1
+        }
+      ], 
+      "Business": [
+        {
+          "host": "bloomberg.com", 
+          "days": 5, 
+          "visits": 114
+        }
+      ], 
+      "Entrepreneur": [
+        {
+          "host": "entrepreneur.com", 
+          "days": 2, 
+          "visits": 11
+        }
+      ]
+    }, 
+    "interestsProfile": [
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 100, 
+        "diversity": 100, 
+        "name": "Business"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 60, 
+        "diversity": 100, 
+        "name": "Politics"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 40, 
+        "diversity": 100, 
+        "name": "Autos"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 40, 
+        "diversity": 100, 
+        "name": "Entrepreneur"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 20, 
+        "diversity": 100, 
+        "name": "Sports"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Android"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Apple"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Arts"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Baseball"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Basketball"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Boxing"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Cooking"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Design"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Do-It-Yourself"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Fashion-Men"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Fashion-Women"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Football"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Gardening"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Golf"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Gossip"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Health-Men"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Health-Women"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Hockey"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Home-Design"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Humor"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Ideas"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Mixed-Martial-Arts"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Movies"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Music"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Parenting"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Photography"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Programming"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Science"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Soccer"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Technology"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Tennis"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Travel"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Television"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Video-Games"
+      }, 
+      {
+        "meta": {
+          "sharable": 1
+        }, 
+        "score": 0, 
+        "diversity": 0, 
+        "name": "Weddings"
+      }
+    ]
+  }, 
+  "type": "pageload"
+}
